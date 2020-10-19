5835c0b59569e43780020f7fdaf7dc75c172a431
Luis Freitas <luisfreitas1999@hotmail.com>
2020-10-19 20:51:14 +0100

    Complete Kepper Aplication.

---
+++ build/precache-manifest.f3bfc971727167b4aa309143d49da372.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4c71dc4726f5054c3e1985d0d9e0ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c1d47a9fd2729ad5dca",
+    "url": "/static/js/2.9060a03d.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/static/js/2.9060a03d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca9ffa434e0d9079e70",
+    "url": "/static/js/main.e767e77e.chunk.js"
+  },
+  {
+    "revision": "ae75c5421005647ef51e",
+    "url": "/static/js/runtime-main.802a07ba.js"
+  }
+]);
